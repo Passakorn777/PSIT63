a412db6caa509f010055bdde625e3606387bb9fb
framejframej6 <71111193+framejframej6@users.noreply.github.com>
2020-12-12 11:45:17 +0700

    Add files via upload

--- python/PRIZE.py
+++ python/PRIZE.py
@@ -1,4 +1,11 @@
 import pandas, numpy, pygal
+from pygal.style import Style
+custom_style = Style(
+  background='#fdf6e3',
+  plot_background='#eee8d5',
+  foreground='#657b83',
+  foreground_strong='#073642',
+  foreground_subtle='#073642',)
 # ref code from https://github.com/tanknk/PSIT-Project/blob/master/python/overallprizemoneybygames.py
 def main():
     """ arrange data """
@@ -7,7 +14,7 @@ def main():
     graph(data)
 def graph(data):
     """ plot graph """
-    line_chart = pygal.Bar(legend_at_bottom=True, legend_at_bottom_columns=5)
+    line_chart = pygal.Bar(legend_at_bottom=True, legend_at_bottom_columns=5,fill=True, interpolate='cubic', style=custom_style)
     line_chart.title = 'Top 20 Games Awarding Prize Money (in Dollars)'
     for i in range(20):
         line_chart.add(str(data[i][0]), [{'value': int(data[i][1]), 'label': '{:.2f}%'.format(100*int(data[i][1])/sum([j[1] for j in data]))}])
--- python/Top20higestEarings.py
+++ python/Top20higestEarings.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import pygal as pg
+from pygal.style import Style
+custom_style = Style(
+  background='#fdf6e3',
+  plot_background='#eee8d5',
+  foreground='#657b83',
+  foreground_strong='#073642',
+  foreground_subtle='#073642',)
 data_20_prize = {}
 
 #ref code from https://github.com/tanknk/PSIT-Project/blob/master/python/Top_20_ProPlayer_Prize_by_prize.py
@@ -10,7 +17,7 @@ for i in range(20):
     data_20_prize[data_top_name[i]] = data_top_prize[i]
 data_20_prize = sorted(data_20_prize.items(), key=lambda x: x[1], reverse=True)
 
-line_chart = pg.HorizontalBar()
+line_chart = pg.HorizontalBar(fill=True, interpolate='cubic', style=custom_style)
 line_chart.title = 'Top 20 Pro Player with the highest reward prize (in Dollars)' # graph name
 for i in data_20_prize:
     line_chart.add(i[0], i[1]) # create
