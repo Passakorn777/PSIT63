c8e76cbd3985eedefc62b001f9ca53402da0b070
framejframej6 <71111193+framejframej6@users.noreply.github.com>
2020-12-12 11:15:27 +0700

    Add files via upload

--- python/PRIZE.py
+++ python/PRIZE.py
@@ -1,4 +1,5 @@
 import pandas, numpy, pygal
+# ref code from https://github.com/tanknk/PSIT-Project/blob/master/python/overallprizemoneybygames.py
 def main():
     """ arrange data """
     dataFrame = pandas.read_csv('https://github.com/Passakorn777/PSIT63/raw/main/data/PRIZE.csv', encoding = "ISO-8859-1")
@@ -6,9 +7,9 @@ def main():
     graph(data)
 def graph(data):
     """ plot graph """
-    line_chart = pygal.Bar(legend_at_bottom=True, legend_at_bottom_columns=4)
-    line_chart.title = 'Top 30 Games Awarding Prize Money (in Dollars)'
-    for i in range(30):
+    line_chart = pygal.Bar(legend_at_bottom=True, legend_at_bottom_columns=5)
+    line_chart.title = 'Top 20 Games Awarding Prize Money (in Dollars)'
+    for i in range(20):
         line_chart.add(str(data[i][0]), [{'value': int(data[i][1]), 'label': '{:.2f}%'.format(100*int(data[i][1])/sum([j[1] for j in data]))}])
     line_chart.render_to_file('PRIZE.svg')  
 
