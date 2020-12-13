77aa12279b46dab1d225d977d979eec16892074e
framejframej6 <71111193+framejframej6@users.noreply.github.com>
2020-12-13 20:40:01 +0700

    Add files via upload

--- python/topgameawardprize.py
+++ python/topgameawardprize.py
@@ -1,4 +1,5 @@
-import pandas, numpy, pygal
+import pandas as pd
+import pygal as pg
 from pygal.style import Style
 custom_style = Style(
   background='#fdf6e3',
@@ -6,18 +7,18 @@ custom_style = Style(
   foreground='#657b83',
   foreground_strong='#073642',
   foreground_subtle='#073642',)
-# ref code from https://github.com/tanknk/PSIT-Project/blob/master/python/overallprizemoneybygames.py
-def main():
-    """ arrange data """
-    dataFrame = pandas.read_csv('https://github.com/Passakorn777/PSIT63/raw/main/data/PRIZE.csv', encoding = "ISO-8859-1")
-    data = numpy.array(dataFrame[['Name','Prize']]).tolist()
-    graph(data)
-def graph(data):
-    """ plot graph """
-    line_chart = pygal.Bar(legend_at_bottom=True, legend_at_bottom_columns=5,fill=True, interpolate='cubic', style=custom_style)
-    line_chart.title = 'Top 20 Games Awarding Prize Money (in Dollars)'
-    for i in range(20):
-        line_chart.add(str(data[i][0]), [{'value': int(data[i][1]), 'label': '{:.2f}%'.format(100*int(data[i][1])/sum([j[1] for j in data]))}])
-    line_chart.render_to_file('TopGameAwardPrize.svg')  
+data_20_topgame_award = {}
 
-main()
+#soure code from https://github.com/tanknk/PSIT-Project/blob/master/python/Top_20_ProPlayer_Prize_by_prize.py
+data = pd.read_csv('https://github.com/Passakorn777/PSIT63/raw/main/data/PRIZE.csv', encoding = "ISO-8859-1") # import file
+data_top_prize = list(data['Prize'])
+data_top_name = list(data['Name'])
+for i in range(20):
+    data_20_topgame_award[data_top_name[i]] = data_top_prize[i]
+data_20_topgame_award = sorted(data_20_topgame_award.items(), key=lambda x: x[1], reverse=True)
+
+line_chart = pg.HorizontalBar(fill=True, interpolate='cubic', style=custom_style)
+line_chart.title = 'Top 20 Games Awarding Prize Money (in Dollars)' # graph name
+for i in data_20_topgame_award:
+    line_chart.add(i[0], i[1]) # create
+line_chart.render_to_file('TopGameAwardPrize.svg') # export file svg
